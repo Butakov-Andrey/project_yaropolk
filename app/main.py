8dbaa83a5948c9e1a9551f53c468fc0161b6a4ea
Andrey Butakov <6669.butakov@gmail.com>
2023-04-04 23:01:20 +0300

    Add html response home page

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from dependencies import logging
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from routers import home_router
 
 app = FastAPI(
@@ -14,6 +16,11 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url=None,
 )
+
+
+# templates
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/")
 
 
 # cors
--- app/routers/home_router.py
+++ app/routers/home_router.py
@@ -1,11 +1,18 @@
+import main
 from dependencies import logging
 from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
 from loguru import logger
 
 home_router = APIRouter()
 
 
-@home_router.get("/", status_code=200, dependencies=[Depends(logging)])
+@home_router.get(
+    "/",
+    status_code=200,
+    response_class=HTMLResponse,
+    dependencies=[Depends(logging)],
+)
 async def home(request: Request) -> dict[str, str]:
     logger.info(f"Request: {request.method} {request.url} {request.headers}")
-    return {"msg": "Welcome to Project Yaropolk"}
+    return main.templates.TemplateResponse("home.html", {"request": request})
