fa7e545827fb0c512bbff212eb05b81307aaed05
Andrey Butakov <6669.butakov@gmail.com>
2023-04-11 09:37:13 +0300

    Update translating service

--- app/services/translating.py
+++ app/services/translating.py
@@ -1,17 +1,23 @@
 from config import settings
-from deep_translator import DeeplTranslator
+from deep_translator import DeeplTranslator, GoogleTranslator
 from deep_translator.exceptions import NotValidPayload
 from fastapi import HTTPException
 
 
 async def translate_text(text: str, from_lang: str, to_lang: str) -> str:
     try:
-        translated_by_deepl = DeeplTranslator(
-            api_key=settings.DEEPL_API_KEY,
-            source=from_lang,
-            target=to_lang,
-            use_free_api=True,
-        ).translate(text)
+        if settings.DEEPL_API_KEY == "test":
+            translated_text = GoogleTranslator(
+                source=from_lang,
+                target=to_lang,
+            ).translate(text)
+        else:
+            translated_text = DeeplTranslator(
+                api_key=settings.DEEPL_API_KEY,
+                source=from_lang,
+                target=to_lang,
+                use_free_api=True,
+            ).translate(text)
     except NotValidPayload as exc:
         raise HTTPException(status_code=400, detail=str(exc))
-    return translated_by_deepl
+    return translated_text
