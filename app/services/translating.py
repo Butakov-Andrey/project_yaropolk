88464741cf144102b55aec0240d24a0e08039446
Andrey Butakov <6669.butakov@gmail.com>
2023-04-11 10:23:22 +0300

    Update trenslate service

--- app/services/translating.py
+++ app/services/translating.py
@@ -6,18 +6,19 @@ from fastapi import HTTPException
 
 async def translate_text(text: str, from_lang: str, to_lang: str) -> str:
     try:
-        if settings.DEEPL_API_KEY == "Deepl APIKey":
-            translated_text = GoogleTranslator(
-                source=from_lang,
-                target=to_lang,
-            ).translate(text)
-        else:
+        if settings.DEEPL_API_KEY != "Deepl APIKey":
             translated_text = DeeplTranslator(
                 api_key=settings.DEEPL_API_KEY,
                 source=from_lang,
                 target=to_lang,
                 use_free_api=True,
             ).translate(text)
+        else:
+            translated_text = GoogleTranslator(
+                source=from_lang,
+                target=to_lang,
+            ).translate(text)
+
     except NotValidPayload as exc:
         raise HTTPException(status_code=400, detail=str(exc))
     return translated_text
